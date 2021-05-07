aebdc56c3c7740a05c31a80862cc58b3f0c14381
anna-nag <karloanna8617@gmail.com>
2021-05-07 17:54:12 +0300

    zadanie 7

---
+++ dz_7.2/dz_7.2/dz_7.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <iterator>
+
+int main()
+{
+	int n;
+	std::fstream fin("7_2.txt", std::ios::in);
+	std::string text;
+	std::getline(fin, text);
+
+	int size = text.size() + 2;
+
+	std::cout << "enter the number of string:" << std::endl;
+	std::cin >> n;
+
+	fin.seekg((n - 1) * size, std::ios::beg);
+	std::getline(fin, text);
+	std::cout << text << std::endl;
+
+	return 0;
+}
